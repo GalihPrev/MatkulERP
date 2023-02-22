ef59d548ebb863cce72b31b2c6ffaa858aa183c0
Galih Previand <galihpreviand27@gmail.com>
2023-02-22 08:20:11 +0700

    first commit

---
+++ praktikum_1/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name"          : "New Addons",
+    "version"       : "1.0",
+    "Author"        : "Galih Previand Wicaksono",
+    "website"       : "https://github.com/GalihPrev",
+    "category"      : "New Modul",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """Latihan Membuat addons baru di odoo 1""",
+    "depends"       : ["base"],
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "images"        : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+    
+}
